7822bdec2f61f35092bc65f56a6206434822a5fa
Takuya Shinozaki <takushino19890913@yahoo.co.jp>
2022-03-12 15:46:45 -0800

    basic setup for chrome extension/react/typescript

---
+++ src/options/options.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const test =  <img src="icon.png" />
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+ReactDOM.render(test, root);
---
+++ src/popup/popup.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './popup.css'
+
+const test = <p>Hello World</p>
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+ReactDOM.render(test, root);
